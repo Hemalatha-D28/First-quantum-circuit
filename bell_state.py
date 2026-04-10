db3d54bd2bc1e8ecdf4117e94cc2a06eb052913f
Hemalatha-D28 <hema.mca17@gmail.com>
2026-04-10 12:30:06 +0530

    Update bell_state.py

--- bell_state.py
+++ bell_state.py
@@ -1,7 +1,12 @@
 from qiskit import QuantumCircuit
 import matplotlib.pyplot as plt
+#Create 2-qubit circuit
 qc=QuantumCircuit(2)
-qc.h(0)
-qc.cx(0,1)
+#step1:Put a qubit 0 in Superposition
+qc.h(0)    #Hadamard gate
+#step2:Entangle qubits
+qc.cx(0,1) #CNOT Gate
+#Draw the quantum circuit diagram
 qc.draw("mpl")
+#Dispaly the circuit visualization
 plt.show()
